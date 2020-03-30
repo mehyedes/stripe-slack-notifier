dcbb036a5e9221dcc49fa5eff0d3b23881b45f31
Mehdi Yedes <mehdi.yedes@gmail.com>
2020-03-30 15:10:06 +0200

    Use python3-http template
    Signed-off-by: Mehdi Yedes <mehdi.yedes@gmail.com>

--- stripe-slack-notifier/handler.py
+++ stripe-slack-notifier/handler.py
@@ -1,49 +1,62 @@
 import stripe
 from babel import numbers
 from slack_webhook import Slack
-from flask import Flask, request
 
 def fetch_secret(secret_name):
     secret_file = open(f"/var/openfaas/secrets/{secret_name}", 'r')
     return secret_file.read()
 
 
-def handle(req):
-    """handle a request to the function
-    Args:
-        req (str): request body
-    """    
+def handle(event, context):
+
+    # Make sure to create the secrets below
+    webhook_url = fetch_secret("slack-webhook-url")
     stripe.api_key = fetch_secret("stripe-secret-key")
     webhook_secret = fetch_secret("webhook-secret")
-    payload = request.data.decode("utf-8")
-    received_sig = request.headers.get("Stripe-Signature", None)
-
-    # Make sure you create a secret named slack-webhook-url with the Webhook URL as value
-    # using the faas CLI command: faas-cli secret create 
-    webhook_url = fetch_secret("slack-webhook-url")
+    
+    payload = event.body
+    received_sig = event.headers.get("Stripe-Signature", None)
+    
     try:
         event = stripe.Webhook.construct_event(
             payload, received_sig, webhook_secret
         )
     except ValueError:
         print("Error while decoding event!")
-        return "Bad payload", 400
+        return {
+            "body": "Bad payload",
+            "statusCode": 400
+        }
     except stripe.error.SignatureVerificationError:
         print("Invalid signature!")
-        return "Bad signature", 400
+        return {
+            "body": "Bad signature", 
+            "statusCode": 400
+        }
+
     # Fail for all other event types  
     if event.type != "charge.succeeded":
-        return "Unsupported event type", 422
+        return {
+            "body":"Unsupported event type",
+            "statusCode": 422
+        }
   
     amount = numbers.format_currency(
       event.data.object.amount / 100,
       event.data.object.currency.upper(), 
       locale='en'
     )
+
     try:
         slack = Slack(url=webhook_url)
         slack.post(text=f"You have a received a new payment of {amount} :moneybag: :tada:")
     except:
         print("An error occured when trying to send slack message.")
-        return "Could not send slack message", 500
-    return "Notification was sent successfully to Slack", 200
+        return {
+            "body": "Could not send slack message", 
+            "statusCode": 500
+        }
+    return {
+        "body": "Notification was sent successfully to Slack", 
+        "statusCode": 200
+    }
